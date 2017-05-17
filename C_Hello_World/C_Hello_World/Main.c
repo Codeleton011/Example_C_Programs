84503f515fe2c30c36146a2cc6f5d2c10a68793c
Codeleton011 <ricsisuper@gmail.com>
2017-05-17 22:28:41 +0200

    Update

--- C_Hello_World/C_Hello_World/Main.c
+++ C_Hello_World/C_Hello_World/Main.c
@@ -1,8 +1,14 @@
-#include <stdio.h>
+// An example C program
+// Author: Codeleton011
+// Date: 2017. 05. 17
 
-int main()
+#include <stdio.h> // Standard input-output header file
+
+int main() // Main
 {
-	printf("Hello World!");
+	printf("Hello World!"); // Prints "Hello World!" to the screen
+
+	getchar(); // Prevent cmd from closing
 
 	return 0;
 }
