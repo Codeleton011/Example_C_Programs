e5d812814701a40d1950a701dd51417b8df60902
Codeleton011 <ricsisuper@gmail.com>
2017-05-21 11:44:46 +0200

    Added C_Pointers

---
+++ C_Pointers/C_Pointers/Main.c
@@ -0,0 +1,25 @@
+// An C Basic Calculator
+// Author: Codeleton011
+// Date: 2017. 05. 21
+
+// Codeleton011
+
+#include <stdio.h>
+
+// Main
+int main()
+{
+	int num = 10;
+
+	int* p = &num;
+
+	printf("Number: %d \n", num);
+
+	printf("Memory address of the number: %d \n", &num);
+
+	printf("A pointer pointing to num: %d \n", *p);
+
+	getchar(); // Prevent cmd closing
+
+	return 0;
+}
