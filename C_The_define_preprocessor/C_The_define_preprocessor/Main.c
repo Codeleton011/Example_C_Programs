e73f7bcca2bca94e61fb2bc073c3c3be443533e9
Codeleton011 <ricsisuper@gmail.com>
2017-05-21 11:16:03 +0200

    Added C_The_define_preprocessor

---
+++ C_The_define_preprocessor/C_The_define_preprocessor/Main.c
@@ -0,0 +1,24 @@
+// An C Basic Calculator
+// Author: Codeleton011
+// Date: 2017. 05. 18
+
+// Codeleton011
+
+#define num1 10
+#define num2 20
+
+#include <stdio.h>
+
+// Main
+int main()
+{
+	int num3;
+
+	num3 = num1 + num2;
+
+	printf("Sum: %d", num3);
+
+	getchar(); // Prevent cmd closing
+
+	return 0;
+}
