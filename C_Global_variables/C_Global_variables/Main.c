0d18289734e8130d8d2a9e55137f39d3841be093
Codeleton011 <ricsisuper@gmail.com>
2017-05-21 11:30:46 +0200

    Added C_Global_variables

---
+++ C_Global_variables/C_Global_variables/Main.c
@@ -0,0 +1,21 @@
+// An C Basic Calculator
+// Author: Codeleton011
+// Date: 2017. 05. 21
+
+// Codeleton011
+
+#include <stdio.h>
+
+int globalVar = 20; // This int is a global variable
+
+// Main
+int main()
+{
+	globalVar = globalVar + 100;
+
+	printf("Sum: %d", globalVar);
+
+	getchar(); // Prevent cmd closing
+
+	return 0;
+}
