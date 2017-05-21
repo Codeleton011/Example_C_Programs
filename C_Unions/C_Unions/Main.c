b92d9c05c1ed117cc7fe5cec497e6b638c79ae78
Codeleton011 <ricsisuper@gmail.com>
2017-05-21 13:12:13 +0200

    Added C_Unions

---
+++ C_Unions/C_Unions/Main.c
@@ -0,0 +1,36 @@
+// An C Basic Calculator
+// Author: Codeleton011
+// Date: 2017. 05. 21
+
+// Codeleton011
+
+#define _CRT_SECURE_NO_WARNINGS // Disable strcpy unsafe error
+#include <stdio.h>
+#include <string.h>
+
+// Union
+union Employee{
+	int sallary;
+	char name[50];
+	int foo;
+};
+
+// Main
+int main()
+{
+	// Access to an union
+	union Employee emp1;
+
+	emp1.sallary = 10000;
+	printf("Employee sallary: %d \n", emp1.sallary);
+
+	strcpy(emp1.name, "John");
+	printf("Employee name: %s \n", emp1.name);
+
+	emp1.foo = 10;
+	printf("Foo: %d \n", emp1.foo);
+
+	getchar(); // Prevent cmd closing
+
+	return 0;
+}
